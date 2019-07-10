a26c8cdc59bc78634484ed2b7a97093bc8622a46
Daniel Hines <d4hines@Daniels-MacBook-Pro.local>
2019-07-10 09:38:10 -0400

    progress!

--- src/fsbl_tester/spec.cljs
+++ src/fsbl_tester/spec.cljs
@@ -1,50 +1,13 @@
 (ns fsbl-tester.spec
   (:require [clojure.spec.alpha :as s :refer [spec]]
             [clojure.spec.gen.alpha :as gen :refer [generate]]
+            [instaparse.core :as insta]
             [clojure.test.check.generators]
             [cljs.pprint :refer [pprint]]
+            [meander.match.delta :as r.match :include-macros true]
             [meander.strategy.delta :as r :include-macros true]
             [expound.alpha]))
-
 (set! s/*explain-out* expound.alpha/printer)
-(def ws
-  {:version "1.0.0"
-   :name "Default Workspace"
-   :type "workspace"
-   :groups {:group1 #{"WC1" "WC2"}}
-   :windows {:WC1 {:componentState {:Finsemble_Linker {:group1 true}}
-                   :windowData {:componentType "Welcome Component"
-                                :defaultHeight 432
-                                :defaultLeft 761
-                                :defaultTop 454
-                                :defaultWidth 400}}
-             :WC2 {:windowData {:componentType "Welcome Component"
-                                :defaultHeight 432
-                                :defaultLeft 1161
-                                :defaultTop 454
-                                :defaultWidth 400}}
-             :Bar {:windowData {:componentType "Welcome Component"
-                                :defaultHeight 432
-                                :defaultLeft 1161
-                                :defaultTop 454
-                                :defaultWidth 400}}
-             :Foo {:windowData {:componentType "Welcome Component"
-                                :defaultHeight 432
-                                :defaultLeft 1161
-                                :defaultTop 454
-                                :defaultWidth 400}}
-             :N {:windowData {:componentType "Notepad"
-                              :defaultHeight 432
-                              :defaultLeft 1161
-                              :defaultTop 454
-                              :defaultWidth 400}}
-             :WPF {:windowData {:componentType "WPFExample"
-                                :defaultHeight 432
-                                :defaultLeft 1161
-                                :defaultTop 454
-                                :defaultWidth 400}}
-             :Stack1 {:windowData {:childWindowIdentifiers #{"Bar" "Foo"}
-                                   :componentType "StackedWindow"}}}})
 
 (defn kw->string [my-map path]
   (update-in my-map path
@@ -91,90 +54,254 @@
                                     ::groups
                                     ::windows]))
 
-(s/def ::add-linker (s/with-gen))
-(s/def ::bounds (s/cat :defaultTop ::bound :defaultLeft ::bound :defaultHeight ::bound :defaultWidth ::bound))
+(defn gen [p] (gen/generate (s/gen p)))
 
-(s/def ::new-window (s/cat
-                     :componentType ::componentType
-                     :name ::name
-                     :bounds ::bounds))
-(s/conform ::new-window ["Notepad" "46gVtzp" 1097 1995 1529 1344])
-(s/def ::new-stack (s/cat
-                    :type #{"Stacked Window" "stacked window"
-                            "Stack of" "stack of"}
-                    :name ::name
-                    :children (s/+ ::new-window)))
+(defn parse [s]
+  (let [cleanStr #(clojure.string/replace %
+                                          #",|and|\:"
+                                          "")
+        parser (insta/parser "
+S = <'Given'?> (window | group)+
 
-(s/conform ::new-stack (gen/generate (s/gen ::new-stack)))
-(s/def ::new-group (s/cat
-                    :type #{"Group" "group"}
-                    :name ::name
-                    :windows (s/+ (s/or
-                                   :stacked-window ::new-stack
-                                   :window ::new-window))))
-(def foo (s/conform ::new-group (gen/generate (s/gen ::new-group))))
+name = #'[a-zA-Z0-9]+'
+number = #'[0-9]+'
+
+window = component name feature*
+component = 'Welcome Component' | 'Notepad'
+feature = bounds | linkerChannel
+bounds = <'with bounds'> number number (number number)?
+linkerChannel = <'on linker channel'> channel
+channel = 'yellow' | 'purple' | 'green' | 'red' 
+
+stack = ('Stacked Window' | 'Stack') name ('with children' | 'of') window window+ <';'?>
+group = <'Group' | 'a group'> <name> <'containing'> (window | stack) (window | stack)+ ';'?
+" :auto-whitespace :standard :string-ci true)]
+    (->> s cleanStr parser (insta/transform {:number int}))))
+
+(def win (parse "
+A Group G1 containing Welcome Component A with bounds 100 200 300 400
+and Welcome Component B;
+Welcome Component FOOBAR
+and Group G1 containing Notepad C and Notepad D"))
+(random-uuid)
+(parse "
+Welcome Component A
+on linker channel yellow
+with bounds 100 200 300 400
+")
 
 
-;; I'm trying to convert between these two formats
-;; First format
+;; There are 3 "kinds" of things:
+;; - Windows
+;; - Stacked Windows, which have 2 or more normal Windows as children
+;; - Groups, which of collections comprised of normal Windows and/or Stacked Windows
+
+;; We have an old and crusty API format that's got tons of duplication, etc.
+;; The task is to parse a controlled natural language into this API format.
 
 
-{:type "Group"
- :name "group1"
- :windows [;; A vec of ::windows, made up of ::componentState
-           ;; and ::windowData
-           {:componentState {:foo :bar}
-            :windowData
-            {:name "Win1"
-             :componentType "Notepad"
-             :bounds {:defaultHeight 100
-                      :defaultWidth 300}}}]}
-;; Second format
-{:windows [;; This is  vec of *just* ::windowData
-           ;; with the :name added.
-           {:name "Win1"
-            :componentType "Notepad"
-            :bounds {:defaultHeight 100
-                     :defaultWidth 300}}]
+(apply merge (r.match/search
+              [[:foo :bar]
+               [:group [:name "foo"]
+                [:window [:name "w1"]]
+                [:window [:component] [:name "w2"]]]
+               [:group [:name "bar"]
+                [:window [:name "w3"] :foo]
+                [:window [:name "w4"]]]]
+              [_ ... [:group
+                      [:name ?group-name]
+                      . [:window . _ ...
+                         [:name !window-name]
+                         . _ ...] ..2]
+               . _ ...]
+              {?group-name {:windowNames !window-name
+                            :isMovable false}
+               (random-uuid) {:windowNames !window-name
+                              :isMovable true}}))
 
- ;; The ::componentState is seperate and indexed by name
- :componentState {"Win1" {:foo :bar}}
- :groups {"group1" {:windows ["Win1"]}}}
+{:windows (r.match/search
+             [:group
+              [:window [:component "WC"] [:name "foo"]
+               [:feature [:bounds 0 100 200 300]]]
+              [:window [:component "WC"] [:name "bar"]]]
+             (scan [:window
+                    [:component ?component]
+                    [:name ?name]
+                    . _ ...
+                    ;; Ideally, this whole feature should be optional
+                    ;; and every bounds should also default to js/undefined
+                    [:feature [:bounds ?top ?left ?width ?height]]
+                    . _ ...])
+             {:componentType ?component
+              :name  ?name
+              :defaultTop ?top
+              :defaultLeft ?left
+              :defaultWidth ?width
+              :defaultHeight ?height})}
 
-{:type "Group"
- :name "group1"
- :windows [{:componentState {:foo :bar}
-            :windowData
-            {:name "Win1"
-             :componentType "Notepad"
-             :bounds {:defaultLeft 2
-                      :defaultHeight 100
-                      :defaultTop 200
-                      :defaultWidth 300}}}]}
+(r.match/search
+ [[:window [:name "w1"] :foobar]
+  [:window [:component] [:name "w2"]]]
+ [_ ... [:window
+         (scan [:name ?window-name])]
 
-(def w [{:type "Group"
-         :name "group1"
-         :windows [{:name "Win1"
-                    :componentType "Notepad"
-                    :bounds {:defaultLeft 2
-                             :defaultHeight 100
-                             :defaultTop 200
-                             :defaultWidth 300}}]}
-        {:type "Group"
-         :name "group2"
-         :windows [{:name "Win1"
-                    :componentType "Notepad"
-                    :bounds {:defaultLeft 2
-                             :defaultHeight 100
-                             :defaultTop 200
-                             :defaultWidth 300}}]}])
+  . _ ...]
+ ?window-name)
 
-"
-?Component ?Name (with bounds ?bounds)* (on linker channel ?channel)
-Group ?Name with windows ?Windows
-Stacked Window ?Name 
-"
-"
-Welcome Component A with boudns 0 0 500 500 on linker channel green
+{:groups {& [[(random-uuid) {:windowNames [!window-name ...]
+                             :isMovable true}] ...]}}
+((r/rewrite
+  [:S [:group [:name ?group-name]
+       . [:window [:name !window-name]
+          [:feature
+           [:bounds !top !left !height !width]
+           [:linkerChannel !linker-channel]]]
+       ...]]
+  {:windows [{:name !window-name
+              :defaultTop !top
+              :defaultLeft !left
+              :defaultHeight !height
+              :defaultWidth !width} ...]})
+ [:S [:group
+      [:name "G1"]
+      [:window
+       [:name "W1"]
+       [:feature
+        [:bounds 100 200 300 400]
+        [:linkerChannel "yellow"]]]
+      [:window
+       [:name "W2"]
+       [:feature
+        [:bounds 100 200 300 400]
+        [:linkerChannel "yellow"]]]]])
 
-"
+((r/rewrite
+  [:S [:group [:name ?group-name]
+       . [:window [:name !window-name]
+          [:feature
+           [:bounds !top !left !height !width]
+           [:linkerChannel !linker-channel]]]
+       ...]]
+  {:windows [{:name !window-name
+              :defaultTop !top
+              :defaultLeft !left
+              :defaultHeight !height
+              :defaultWidth !width} ...]
+   :componentState {& [[!window-name
+                        {:Finsemble_Linker
+                         {& [[!linker-channel true] ...]}}]
+                       ...]}})
+ [:S [:group
+      [:name "G1"]
+      [:window
+       [:name "W1"]
+       [:feature
+        [:bounds 100 200 300 400]
+        [:linkerChannel "yellow"]]]
+      [:window
+       [:name "W2"]
+       [:feature
+        [:bounds 100 200 300 400]
+        [:linkerChannel "yellow"]]]]])
+(comment
+
+  ((r/rewrite
+    [:S [:group [:name ?group-name]
+         . [:window [:name !window-name]
+            [:feature
+             [:bounds !top !left !height !width]
+             [:linkerChannel !linker-channel]]]
+         ...]]
+
+    {:groups #{?group-name {:windowNames [!window-name ...]}
+               :isMovable true}
+
+     :windows [{:name !window-name
+                :defaultTop !top
+                :defaultLeft !left
+                :defaultHeight !height
+                :defaultWidth !width} ...]
+     :componentState {& [[!window-name
+                          {:Finsemble_Linker
+                           {& [[!linker-channel true] ...]}}]
+                         ...]}})
+   [:S [:group
+        [:name "G1"]
+        [:window
+         [:name "W1"]
+         [:feature
+          [:bounds 100 200 300 400]
+          [:linkerChannel "yellow"]]]
+        [:window
+         [:name "W2"]
+         [:feature
+          [:bounds 100 200 300 400]
+          [:linkerChannel "yellow"]]]]])
+
+  (-> (parse "
+Given:
+Welcome Component A with bounds 0, 100, 200, 300, and on linker channel yellow,
+Notepad B with bounds 100 and 200,
+Group G1 containing Welcome Component C and Notepad D with bounds 100, 200, 300, 444
+and Stacked Window S1 with children Welcome Component WCS1 and Welcome Component WCS2")
+      (nth 3))
+
+  (r/search
+   [:S [:group
+        [:name "G1"]
+        [:window
+         [:name "W1"]
+         [:feature
+          [:bounds 100 200 300 400]
+          [:linkerChannel "yellow"]]]]]
+   [:S
+    [:group [:name ?group-name]
+     & (r.match/scan
+        [:window [:name ?window-name]
+         & (r.match/scan [:feature & (r.match/scan [:bounds ?top ?left ?height ?width])])])]]
+   [?group-name ?window-name [:bounds ?top ?left ?height ?width]])
+
+  ((r/rewrite
+    [:S [:group [:name ?group-name]
+         . [:window [:name !window-name]
+            (or [:feature
+                 [:bounds !top !left !height !width]
+                 [:linkerChannel !linker-channel]]
+
+                (and [:feature
+                      [:bounds !top !left !height !width]]
+                     (m/let !linker-channel "default-channel"))
+
+                (and [:feature
+                      [:linkerChannel !linker-channel]]
+                     (m/let [!top !left !height !width] [0 0 100 100]))
+
+                (let [!top !left !height !width] [0 0 100 100]))]
+
+         ...]]
+
+    {:groups {?group-name ~(set !window-name)}
+     :windows {& [[!window-name {:defaultTop !top
+                                 :defaultLeft !left
+                                 :defaultHeight !height
+                                 :defaultWidth !width}] ...]}})
+   [:S [:group
+        [:name "G1"]
+        [:window
+         [:name "W1"]
+         [:feature
+          [:bounds 100 200 300 400]
+          [:linkerChannel "yellow"]]]
+        [:window
+         [:name "W2"]
+         [:feature
+          [:bounds 100 200 300 400]]]
+        [:window
+         [:name "W3"]
+         [:feature
+          [:linkerChannel "yellow"]]]
+        [:window
+         [:name "W4"]
+         [:feature
+          [:linkerChannel "yellow"]]]]]))
+
